8ebda579b54c4f23def58e85141ed51adf6a1ddf
Woshiwuja <gabusif@outlook.it>
2025-02-07 00:14:16 +0100

    lesgo

---
+++ src/lua-functions.zig
@@ -0,0 +1,10 @@
+// implementing functions that can be called from lua.
+const ziglua = @import("ziglua");
+
+pub fn OnUpdate() void {}
+
+pub fn OnTrigger() void {}
+
+pub fn GetEntityDataBy() !void {}
+
+pub fn OnKeyPress() !void {}
