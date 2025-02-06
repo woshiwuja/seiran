4becf27f108d01d917cf97d47b6fc3c4ff8608af
Woshiwuja <gabusif@outlook.it>
2025-02-07 00:13:32 +0100

    update

---
+++ src/entity/script.lua
@@ -0,0 +1,5 @@
+local script = {}
+
+function OnUpdate()
+    speed = 
+end
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,34 @@
 const std = @import("std");
 const rl = @import("raylib");
 const rg = @import("raygui");
+const ziglua = @import("ziglua");
 //const clay = @cImport({
 //    @cInclude("src/clay.h");
 //});
 const print = std.debug.print;
+
+const cString = @cImport({
+    @cInclude("string.h");
+});
+
+fn strdup(str: [:0]const u8) ![*:0]u8 {
+    return cString.strdup(str) orelse error.OutOfMemory;
+}
+
+fn loadMap(image: [*:0]const u8) !rl.Model {
+    const heightMapImage = try rl.loadImage(image);
+    const heightMapTexture = try rl.loadTextureFromImage(heightMapImage);
+    defer rl.unloadTexture(heightMapTexture);
+    const meshSize: rl.Vector3 = .{ .x = 200, .y = 45, .z = 190 };
+    const mesh = rl.genMeshHeightmap(heightMapImage, meshSize);
+    _ = rl.exportMesh(mesh, "map");
+    var map = try rl.loadModelFromMesh(mesh);
+    map.materials[0].maps[0].texture = heightMapTexture;
+    rl.unloadImage(heightMapImage);
+    return map;
+}
+
+fn initLua() !void {}
 
 //should come from settings file
 fn initWindowSettings() void {
@@ -23,39 +47,53 @@ fn initDefaultCamera() rl.Camera3D {
     return camera;
 }
 
+fn drawMap(map: rl.Model, position: rl.Vector3, scale: f32, tint: rl.Color, water: bool) void {
+    if (water) {
+        const waterLevel = rl.Vector3{ .x = -8, .y = 10, .z = -8 };
+        const waterSize: rl.Vector2 = .{ .x = 3000, .y = 3000 };
+        rl.drawPlane(waterLevel, waterSize, rl.Color.blue);
+    }
+    rl.drawModel(map, position, scale, tint);
+    rl.drawGrid(2000, 1);
+    return;
+}
+
+const screenWidth: i32 = 1920;
+const screenHeight: i32 = 1080;
 pub fn main() !void {
-    const screenWidth: i32 = 1920;
-    const screenHeight: i32 = 1080;
     rl.initWindow(screenWidth, screenHeight, "Steel Shell");
+    defer rl.closeWindow();
     initWindowSettings();
     var camera = initDefaultCamera();
-    const heightMapImage = try rl.loadImage("assets/monzuno.png");
-    const heightMapTexture = try rl.loadTextureFromImage(heightMapImage);
-    defer rl.unloadTexture(heightMapTexture);
-    const meshSize: rl.Vector3 = .{ .x = 384, .y = 45, .z = 190 };
-    const mesh = rl.genMeshHeightmap(heightMapImage, meshSize);
-    _ = rl.exportMesh(mesh, "map");
-    var map = try rl.loadModelFromMesh(mesh);
-    map.materials[0].maps[0].texture = heightMapTexture;
-    const mapPosition = rl.Vector3{ .x = -8, .y = 4, .z = -8 };
-    const water = rl.Vector3{ .x = -8, .y = 10, .z = -8 };
-    const waterSize: rl.Vector2 = .{ .x = 3000, .y = 3000 };
-    rl.unloadImage(heightMapImage);
-    defer rl.closeWindow();
+    const map = try loadMap("assets/monzuno.png");
+    const mapPosition = rl.Vector3{ .x = 0, .y = 4, .z = 0 };
+    const text = "hello";
+    const textCopy = try strdup(text);
+    defer std.c.free(textCopy);
+    var cursorEnabled = false;
+    rl.updateCamera(&camera, rl.CameraMode.free);
     while (!rl.windowShouldClose()) {
         rl.updateCamera(&camera, rl.CameraMode.free);
+        if (rl.isKeyPressed(rl.KeyboardKey.escape)) {
+            if (!cursorEnabled) {
+                rl.enableCursor();
+                rl.updateCamera(&camera, rl.CameraMode.orbital);
+                cursorEnabled = !cursorEnabled;
+            } else {
+                rl.disableCursor();
+                cursorEnabled = !cursorEnabled;
+                rl.updateCamera(&camera, rl.CameraMode.free);
+            }
+        }
         rl.beginDrawing();
-        rl.clearBackground(rl.Color.ray_white);
+        //DRAWING
+        rl.clearBackground(rl.Color.white);
         rl.beginMode3D(camera);
-        rl.drawModel(map, mapPosition, 1, rl.Color.brown);
-        rl.drawGrid(2000, 1);
-        rl.drawPlane(water, waterSize, rl.Color.sky_blue);
+        drawMap(map, mapPosition, 1, rl.Color.brown, true);
         rl.endMode3D();
-        const windowBoxRect = rl.Rectangle{ .height = 200, .width = 300, .x = 40, .y = 40 };
-        _ = rg.guiWindowBox(windowBoxRect, "finestra");
         rl.drawFPS(100, 100);
-        //rl.drawTexture(heightMapTexture, screenWidth - heightMapTexture.width - 20, 20, rl.Color.white);
-        //rl.drawRectangleLines(screenWidth - heightMapTexture.width - 20, 20, heightMapTexture.width, heightMapTexture.height, rl.Color.green);
+        _ = rg.guiMessageBox(rl.Rectangle{ .height = 400, .width = 800, .x = -40, .y = -40 }, "hello", "hello", "bottone");
+        //END DRAWING
         rl.endDrawing();
     }
     rl.unloadModel(map);
